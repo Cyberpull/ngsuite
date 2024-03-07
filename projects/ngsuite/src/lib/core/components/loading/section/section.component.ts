ca10acf6856c868a22fe8b652dae433864975fd4
Christian Ezeani <christian.ezeani@gmail.com>
2024-03-07 16:01:48 +0100

    Remove `LoadingWriter` and `LoadingReader`

--- projects/ngsuite/src/lib/core/components/loading/section/section.component.ts
+++ projects/ngsuite/src/lib/core/components/loading/section/section.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, ChangeDetectorRef, Component, ComponentRef, Inject, Injector, Input, ViewChild, ViewContainerRef } from "@angular/core";
 import { NGSuiteConfig } from "../../../interfaces";
 import { NGSuiteLoadingAnimationComponent } from "../animation/animation.component";
-import { LoadingReader, LoadingWriter } from "../../../services";
 
 @Component({
   selector: 'ngs-loading-section',
@@ -11,8 +10,6 @@ import { LoadingReader, LoadingWriter } from "../../../services";
 export class NGSuiteLoadingSectionComponent implements AfterViewInit {
 
   private componentRef?: ComponentRef<any>;
-
-  private writer: LoadingWriter;
 
   @ViewChild('container', {
     read: ViewContainerRef
@@ -24,21 +21,17 @@ export class NGSuiteLoadingSectionComponent implements AfterViewInit {
     private injector: Injector,
     private cd: ChangeDetectorRef,
     @Inject('NGSuite') private config: NGSuiteConfig
-  ) {
-    this.writer = new LoadingWriter();
-  }
+  ) {  }
 
   ngAfterViewInit() {
-    const { writer, viewContainerRef, config, cd } = this;
+    const { viewContainerRef, config, cd } = this;
 
     if (viewContainerRef) {
       const animation = config.sectionLoadingAnimation || config.loadingAnimation || NGSuiteLoadingAnimationComponent;
 
       const newInjector = Injector.create({
         parent: this.injector,
-        providers: [
-          { provide: LoadingReader, useValue: new LoadingReader(writer) }
-        ]
+        providers: []
       });
 
       this.componentRef = viewContainerRef.createComponent(animation, {
--- projects/ngsuite/src/lib/core/services/index.ts
+++ projects/ngsuite/src/lib/core/services/index.ts
@@ -1,4 +1,2 @@
 export * from './Loading';
-export * from './LoadingWriter';
-export * from './LoadingReader';
 export * from './RouteLoading';
