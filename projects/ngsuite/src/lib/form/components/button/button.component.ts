352a2a26cc143b8872a3230ff4d9205f90abe352
Christian Ezeani <christian.ezeani@gmail.com>
2025-06-17 15:58:35 +0100

    Fix dialog footer and buttons

--- projects/ngsuite/src/lib/form/components/button/button.component.ts
+++ projects/ngsuite/src/lib/form/components/button/button.component.ts
@@ -1,5 +1,11 @@
-import { Component, ElementRef, Input } from "@angular/core";
+import { Component, computed, ElementRef, HostBinding, inject, input, Input } from "@angular/core";
 import { Icon, NGSuiteIconComponent } from "../../../core";
+import { NgClass } from "@angular/common";
+
+type ButtonDirection = 'normal' | 'reversed';
+type ButtonTheme = 'default' | 'primary' | 'secondary' | 'error' | 'danger' | 'success' | 'warning' | 'info' | 'light' | 'dark';
+type ButtonType = 'submit' | 'button' | 'reset' | 'menu';
+type ButtonSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
 @Component({
   selector: 'ngs-button',
@@ -7,20 +13,49 @@ import { Icon, NGSuiteIconComponent } from "../../../core";
   styleUrls: ['button.component.scss'],
   standalone: true,
   imports: [
+    NgClass,
     NGSuiteIconComponent,
   ],
 })
 export class NGSuiteFormButtonComponent {
 
-  @Input() icon: Icon = '';
-  @Input() type: string = 'button';
-  @Input() disabled: boolean = false;
+  private readonly ref: ElementRef<HTMLElement> = inject(ElementRef);
 
-  constructor(
-    private el: ElementRef<HTMLElement>
-  ) {
-    const { nativeElement: $element } = el;
-    $element.tabIndex = 1;
+  readonly icon = input<Icon>();
+  readonly theme = input<ButtonTheme>('default');
+  readonly type = input<ButtonType>('button');
+  readonly size = input<ButtonSize>('md');
+  readonly direction = input<ButtonDirection>('normal');
+
+  readonly block = input<boolean>(false);
+  readonly disabled = input<boolean>(false);
+
+  readonly btnClassNames = computed(() => {
+    const names: string[] = [];
+
+    const direction = this.direction();
+    if (direction === 'reversed') names.push(direction);
+
+    return names;
+  });
+
+  readonly classNames = computed(() => {
+    const names: string[] = [this.theme(), this.size()];
+    return names.join(' ');
+  });
+
+  @HostBinding('class')
+  get bindClass() { return this.classNames(); }
+
+  @HostBinding('class.block')
+  get isBlock() { return this.block(); }
+
+  @HostBinding('class.disabled')
+  get isDisabled() { return this.disabled(); }
+
+  constructor() {
+    const { ref: { nativeElement } } = this;
+    nativeElement.tabIndex = 1;
   }
 
 }
